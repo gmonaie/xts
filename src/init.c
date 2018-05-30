6b87fc9f0945782c35d2419b947c49bbba42b8cf
Joshua Ulrich <josh.m.ulrich@gmail.com>
2018-05-30 09:55:56 -0500

    WIP .indexFORMAT -> tformat

--- src/init.c
+++ src/init.c
@@ -67,7 +67,7 @@ static void SymbolShortcuts(void)
 {
   xts_IndexSymbol = install("index");
   xts_ClassSymbol = install(".CLASS");
-  xts_IndexFormatSymbol = install(".indexFORMAT");
+  xts_IndexTformatSymbol = install("tformat");
   xts_IndexTclassSymbol = install("tclass");
   xts_IndexTzoneSymbol = install("tzone");
 }
